51d95025e794ab5807fb6751d1926d95227b1482
Abubakr <abubakrwindowz@gmail.com>
2026-02-06 19:46:25 +0300

    added favourite field to notes object

--- lib/note_item.dart
+++ lib/note_item.dart
@@ -3,16 +3,24 @@ class NoteItem {
   final String title;
   final String content;
   final String date;
+  final bool favourite; // Must be here!
 
-  NoteItem({required this.id, required this.title, required this.content, required this.date});
+  NoteItem({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.date,
+    required this.favourite,
+  });
 
-  // Factory to convert JSON from Backend to a Dart Object
   factory NoteItem.fromJson(Map<String, dynamic> json) {
     return NoteItem(
-      id: json['id'],
+      id: json['id'].toString(),
       title: json['title'] ?? "",
-      content: json['body'] ?? "", // Backend uses 'body'
-      date: json['updated_at'] ?? json['created_at'] ?? "",
+      content: json['body'] ?? "", // Map 'body' from DB to 'content' in Flutter
+      date: json['updated_at'] ?? "",
+      // This is the critical line:
+      favourite: json['favourite'] == 1 || json['favourite'] == true,
     );
   }
 }
